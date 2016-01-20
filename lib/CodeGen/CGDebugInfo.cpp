1ccef33e2a7ced49058ac9041d192a8c07969261
Adrian Prantl <aprantl@apple.com>
2016-01-20 01:29:34 +0000

    Module Debugging: Fine-tune the condition that determines whether a type can be found in a module.
    There are externally visible anonymous types that can be found:
      typedef struct { } s; // I can be found via the typedef.
    There are anonymous internal types that can be found:
      namespace { struct s {}; } // I can be found by name.
    
    rdar://problem/24199640
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@258272 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 3d5d4c39659f11dfbe8e11c857cadf5c449b559b)

--- lib/CodeGen/CGDebugInfo.cpp
+++ lib/CodeGen/CGDebugInfo.cpp
@@ -1528,7 +1528,7 @@ static bool shouldOmitDefinition(CodeGenOptions::DebugInfoKind DebugKind,
                                  const LangOptions &LangOpts) {
   // Does the type exist in an imported clang module?
   if (DebugTypeExtRefs && RD->isFromASTFile() && RD->getDefinition() &&
-      RD->isExternallyVisible())
+      (RD->isExternallyVisible() || !RD->getName().empty()))
     return true;
 
   if (DebugKind > CodeGenOptions::LimitedDebugInfo)

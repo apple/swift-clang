b88f23b3c59960269793a4cac7887a5884b55cdf
Manman Ren <mren@apple.com>
2016-04-15 16:31:49 -0700

    Revert "[modules] Flatten -fmodule-name= and -fmodule-implementation-of= into a single"
    This reverts commit 38d1bfb97b6d95abb1af5485439f73c3b99cef34.
    
    (cherry picked from commit 0258e9f31d659ba86223651d1896059dca9a274a)

--- lib/Lex/PPMacroExpansion.cpp
+++ lib/Lex/PPMacroExpansion.cpp
@@ -1441,9 +1441,8 @@ static bool EvaluateBuildingModule(Token &Tok,
     return false;
   }
 
-  bool Result =
-      PP.getLangOpts().CompilingModule &&
-      Tok.getIdentifierInfo()->getName() == PP.getLangOpts().CurrentModule;
+  bool Result
+    = Tok.getIdentifierInfo()->getName() == PP.getLangOpts().CurrentModule;
 
   // Get ')'.
   PP.LexNonComment(Tok);

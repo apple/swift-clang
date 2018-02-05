09d35965e9e0ffb959b3a77bb1053dbdb686c80d
asowani <sowania@us.ibm.com>
2018-02-05 17:27:24 +0530

    ppc64le port.

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -1777,6 +1777,15 @@ public:
     }
     return false;
   }
+
+  CallingConvCheckResult checkCallingConvention(CallingConv CC) const override {
+    switch (CC) {
+      case CC_Swift:
+        return CCCR_OK;
+      default:
+        return CCCR_Warning;
+    }
+  }
 };
 
 class DarwinPPC32TargetInfo : public DarwinTargetInfo<PPC32TargetInfo> {

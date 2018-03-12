1da4299dcc278446f356876e843423b55e4d9936
Alex Lorenz <arphaman@gmail.com>
2018-03-12 19:36:29 +0000

    [Tooling] Clear the PreambleSrcLocCache when preamble is discarded during reparsing
    This ensures that diagnostics are not remapped to incorrect preamble locations after
    the second reparse with a remapped header file occurs.
    
    rdar://37502480
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@327322 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 07fe671aaad3e93b9f7178ae1309304a73a3b3bf)

--- lib/Frontend/ASTUnit.cpp
+++ lib/Frontend/ASTUnit.cpp
@@ -1250,6 +1250,7 @@ ASTUnit::getMainBufferWithPrecompiledPreamble(
       Preamble.reset();
       PreambleDiagnostics.clear();
       TopLevelDeclsInPreamble.clear();
+      PreambleSrcLocCache.clear();
       PreambleRebuildCounter = 1;
     }
   }

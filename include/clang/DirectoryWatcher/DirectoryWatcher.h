26285043fe9be84c71bad28f55d1248059604294
Jan Korous <jkorous@apple.com>
2019-03-14 15:30:32 -0700

    [DirectoryWatcher][NFC] Remove duplicated comment

--- include/clang/DirectoryWatcher/DirectoryWatcher.h
+++ include/clang/DirectoryWatcher/DirectoryWatcher.h
@@ -3,11 +3,6 @@
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-/// \file
-/// \brief Utility class for listening for file system changes in a directory.
-//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_CLANG_DIRECTORYWATCHER_DIRECTORYWATCHER_H
 #define LLVM_CLANG_DIRECTORYWATCHER_DIRECTORYWATCHER_H
